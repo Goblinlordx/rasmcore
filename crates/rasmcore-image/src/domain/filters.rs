5725a0837fcfd5d59685bfa74a803a30e7b23ee0
Ben Baldivia <goblinlordx@gmail.com>
2026-03-31 23:38:46 +0900

    refactor: migrate evaluate_max to rect-request signature

--- crates/rasmcore-image/src/domain/filters.rs
+++ crates/rasmcore-image/src/domain/filters.rs
@@ -2341,10 +2341,22 @@ pub fn evaluate_min(pixels: &[u8], info: &ImageInfo, config: &EvaluateMinParams)
     group = "evaluate", variant = "max",
     reference = "ImageMagick -evaluate Max", point_op = "true"
 )]
-pub fn evaluate_max(pixels: &[u8], info: &ImageInfo, config: &EvaluateMaxParams) -> Result<Vec<u8>, ImageError> {
+pub fn evaluate_max(
+    request: Rect,
+    upstream: &mut UpstreamFn,
+    info: &ImageInfo,
+    config: &EvaluateMaxParams,
+) -> Result<Vec<u8>, ImageError> {
+    let pixels = upstream(request)?;
+    let info = &ImageInfo { width: request.width, height: request.height, ..*info };
+    let pixels = pixels.as_slice();
     validate_format(info.format)?;
     if is_16bit(info.format) {
-        return process_via_8bit(pixels, info, |p8, i8| evaluate_max(p8, i8, config));
+        return process_via_8bit(pixels, info, |p8, i8| {
+            let r = Rect::new(0, 0, i8.width, i8.height);
+            let mut u = |_: Rect| Ok(p8.to_vec());
+            evaluate_max(r, &mut u, i8, config)
+        });
     }
     let lut = super::point_ops::build_lut(&super::point_ops::PointOp::EvalMax(config.value));
     super::point_ops::apply_lut(pixels, info, &lut)
