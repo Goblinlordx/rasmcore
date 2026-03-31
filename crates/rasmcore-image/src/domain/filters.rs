35f718d234e26137714d6b58076c9b45f4b0cb65
Ben Baldivia <goblinlordx@gmail.com>
2026-03-31 23:39:14 +0900

    refactor: migrate evaluate_min to rect-request signature

--- crates/rasmcore-image/src/domain/filters.rs
+++ crates/rasmcore-image/src/domain/filters.rs
@@ -2326,10 +2326,17 @@ pub fn evaluate_divide(
     group = "evaluate", variant = "min",
     reference = "ImageMagick -evaluate Min", point_op = "true"
 )]
-pub fn evaluate_min(pixels: &[u8], info: &ImageInfo, config: &EvaluateMinParams) -> Result<Vec<u8>, ImageError> {
+pub fn evaluate_min(request: Rect, upstream: &mut UpstreamFn, info: &ImageInfo, config: &EvaluateMinParams) -> Result<Vec<u8>, ImageError> {
+    let pixels = upstream(request)?;
+    let info = &ImageInfo { width: request.width, height: request.height, ..*info };
+    let pixels = pixels.as_slice();
     validate_format(info.format)?;
     if is_16bit(info.format) {
-        return process_via_8bit(pixels, info, |p8, i8| evaluate_min(p8, i8, config));
+        return process_via_8bit(pixels, info, |p8, i8| {
+            let r = Rect::new(0, 0, i8.width, i8.height);
+            let mut u = |_: Rect| Ok(p8.to_vec());
+            evaluate_min(r, &mut u, i8, config)
+        });
     }
     let lut = super::point_ops::build_lut(&super::point_ops::PointOp::EvalMin(config.value));
     super::point_ops::apply_lut(pixels, info, &lut)
