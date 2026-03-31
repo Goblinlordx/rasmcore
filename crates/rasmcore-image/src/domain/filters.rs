916224ed4280913a536606728b4177a5fdd30956
Ben Baldivia <goblinlordx@gmail.com>
2026-03-31 23:35:42 +0900

    refactor: migrate evaluate_log to rect-request signature

--- crates/rasmcore-image/src/domain/filters.rs
+++ crates/rasmcore-image/src/domain/filters.rs
@@ -2359,10 +2359,22 @@ pub fn evaluate_pow(pixels: &[u8], info: &ImageInfo, config: &EvaluatePowParams)
     group = "evaluate", variant = "log",
     reference = "ImageMagick -evaluate Log", point_op = "true"
 )]
-pub fn evaluate_log(pixels: &[u8], info: &ImageInfo, config: &EvaluateLogParams) -> Result<Vec<u8>, ImageError> {
+pub fn evaluate_log(
+    request: Rect,
+    upstream: &mut UpstreamFn,
+    info: &ImageInfo,
+    config: &EvaluateLogParams,
+) -> Result<Vec<u8>, ImageError> {
+    let pixels = upstream(request)?;
+    let info = &ImageInfo { width: request.width, height: request.height, ..*info };
+    let pixels = pixels.as_slice();
     validate_format(info.format)?;
     if is_16bit(info.format) {
-        return process_via_8bit(pixels, info, |p8, i8| evaluate_log(p8, i8, config));
+        return process_via_8bit(pixels, info, |p8, i8| {
+            let r = Rect::new(0, 0, i8.width, i8.height);
+            let mut u = |_: Rect| Ok(p8.to_vec());
+            evaluate_log(r, &mut u, i8, config)
+        });
     }
     let lut = super::point_ops::build_lut(&super::point_ops::PointOp::EvalLog(config.base));
     super::point_ops::apply_lut(pixels, info, &lut)
