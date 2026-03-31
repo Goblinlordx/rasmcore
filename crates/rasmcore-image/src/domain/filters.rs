1cc317369d4469fde1c90739dbc852fa7dc462a4
Ben Baldivia <goblinlordx@gmail.com>
2026-03-31 23:39:13 +0900

    refactor: migrate evaluate_pow to rect-request signature

--- crates/rasmcore-image/src/domain/filters.rs
+++ crates/rasmcore-image/src/domain/filters.rs
@@ -2368,10 +2368,17 @@ pub fn evaluate_max(
     group = "evaluate", variant = "pow",
     reference = "ImageMagick -evaluate Pow", point_op = "true"
 )]
-pub fn evaluate_pow(pixels: &[u8], info: &ImageInfo, config: &EvaluatePowParams) -> Result<Vec<u8>, ImageError> {
+pub fn evaluate_pow(request: Rect, upstream: &mut UpstreamFn, info: &ImageInfo, config: &EvaluatePowParams) -> Result<Vec<u8>, ImageError> {
+    let pixels = upstream(request)?;
+    let info = &ImageInfo { width: request.width, height: request.height, ..*info };
+    let pixels = pixels.as_slice();
     validate_format(info.format)?;
     if is_16bit(info.format) {
-        return process_via_8bit(pixels, info, |p8, i8| evaluate_pow(p8, i8, config));
+        return process_via_8bit(pixels, info, |p8, i8| {
+            let r = Rect::new(0, 0, i8.width, i8.height);
+            let mut u = |_: Rect| Ok(p8.to_vec());
+            evaluate_pow(r, &mut u, i8, config)
+        });
     }
     let lut = super::point_ops::build_lut(&super::point_ops::PointOp::EvalPow(config.exponent));
     super::point_ops::apply_lut(pixels, info, &lut)
