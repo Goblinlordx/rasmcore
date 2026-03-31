8de92626a67d53e81fdba70b2c09b37a30ff5bc1
Ben Baldivia <goblinlordx@gmail.com>
2026-03-31 23:38:33 +0900

    refactor: migrate evaluate_divide to rect-request signature

--- crates/rasmcore-image/src/domain/filters.rs
+++ crates/rasmcore-image/src/domain/filters.rs
@@ -2299,10 +2299,22 @@ pub fn evaluate_multiply(request: Rect, upstream: &mut UpstreamFn, info: &ImageI
     group = "evaluate", variant = "divide",
     reference = "ImageMagick -evaluate Divide", point_op = "true"
 )]
-pub fn evaluate_divide(pixels: &[u8], info: &ImageInfo, config: &EvaluateDivideParams) -> Result<Vec<u8>, ImageError> {
+pub fn evaluate_divide(
+    request: Rect,
+    upstream: &mut UpstreamFn,
+    info: &ImageInfo,
+    config: &EvaluateDivideParams,
+) -> Result<Vec<u8>, ImageError> {
+    let pixels = upstream(request)?;
+    let info = &ImageInfo { width: request.width, height: request.height, ..*info };
+    let pixels = pixels.as_slice();
     validate_format(info.format)?;
     if is_16bit(info.format) {
-        return process_via_8bit(pixels, info, |p8, i8| evaluate_divide(p8, i8, config));
+        return process_via_8bit(pixels, info, |p8, i8| {
+            let r = Rect::new(0, 0, i8.width, i8.height);
+            let mut u = |_: Rect| Ok(p8.to_vec());
+            evaluate_divide(r, &mut u, i8, config)
+        });
     }
     let lut = super::point_ops::build_lut(&super::point_ops::PointOp::EvalDivide(config.factor));
     super::point_ops::apply_lut(pixels, info, &lut)
